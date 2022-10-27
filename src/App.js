7427fb4b7cca586fbc9250ebbf632bddc73ecc44
mahawar0709 <78439325+mahawar0709@users.noreply.github.com>
2022-10-27 23:45:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NotesList from './components/NotesList'
+
+export default function App() {
+  return (
+    <div className='container'>
+      <NotesList/>
+    </div>
+  )
+}
